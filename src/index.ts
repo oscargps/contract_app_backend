09e92159ad15a3bc170bbdc3b642de416c4927bd
Oscar Pastrana <opastra@bancodebogota.com.co>
2024-08-17 11:47:50 -0500

    ✨ ADD DEBUGGING AND ENV VARIABLES SUPPORT

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express from 'express';
 import healthRouter from './modules/core/router/health.router';
-const app = express();
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true }))
-app.use('/',healthRouter );
+import { getVariable } from './config';
+var debug = require('debug')('contractApp:launchServer');
 
 
-app.listen(3000, () => {
-  console.log('Server listening in port 3000');
-});
+(async () => {
+  try {
+    const port = await getVariable('PORT');
+    const app = express();
+    app.use(express.json()) // for parsing application/json
+    app.use(express.urlencoded({ extended: true }))
+    app.use('/', healthRouter);
+
+    app.set('port', port);
+    debug(`Port set to ${port}`);
+
+    debug('Server created');
+
+
+    app.listen(port);
+  } catch (error) {
+    debug(`[ERROR] Could not start application: ${error}`);
+  }
+})();
